1901e9f55a3a3df1e4bc402a9767eb543c7c4f33
topzz100 <adenekantope@gmail.com>
2022-07-05 05:29:15 +0100

    slider in progress

--- src/components/list/List.tsx
+++ src/components/list/List.tsx
@@ -1,13 +1,30 @@
 import './list.scss'
 import ListItem from '../listItem/ListItem'
+import { useRef } from 'react'
 
 const List = () => {
+  const wrapperRef = useRef<HTMLInputElement | null>(null)
+  const handleSlide = (dir: string) => {
+    if (null !== wrapperRef.current){
+
+      const distance = wrapperRef.current.getBoundingClientRect().x
+      if(dir === 'left'){
+        wrapperRef.current.style.transform = `translateX(${distance-225- 50 -10}px)`
+        console.log(wrapperRef.current.getBoundingClientRect())
+      }
+      if(dir === 'right'){
+        wrapperRef.current.style.transform = `translateX(${distance +225-50+10}px)`
+        console.log(distance)
+      }
+    }
+    
+  }
   return (
     <div className='list'>
       <h4>Continue to watch</h4>
       <div className="container">
-
-        <div className="wrapper">
+        <i className="fa-solid fa-chevron-left left" onClick={() => handleSlide('left')}></i>
+        <div className="wrapper" ref={wrapperRef}>
           <div className="listItem">
             <ListItem/>
           </div>
@@ -36,6 +53,7 @@ const List = () => {
             <ListItem/>
           </div>
         </div>
+        <i className="fa-solid fa-chevron-right right" onClick={() => handleSlide('right')}></i>
       </div>
     </div>
   )
